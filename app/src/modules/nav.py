cfb816cc827acad6a4fddbe684061934354c548a
josh-segal <122911717+josh-segal@users.noreply.github.com>
2024-05-31 11:18:15 +0200

    adds home button when on about page

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -11,7 +11,7 @@ def AboutPageNav():
 
 #### ------------------------ Examples for Role of pol_strat_advisor ------------------------
 def PolStratAdvHomeNav():
-    st.sidebar.page_link("pages/00_Pol_Strat_Home.py", label="Political Strategist Home", icon='👤')
+    st.sidebar.page_link("pages/00_Investor_Home.py", label="Political Strategist Home", icon='👤')
 
 def WorldBankVizNav():
     st.sidebar.page_link("pages/01_World_Bank_Viz.py", label="World Bank Visualization", icon='🏦')
@@ -31,7 +31,7 @@ def ClassificationNav():
 
 #### ------------------------ System Admin Role ------------------------
 def AdminPageNav():
-    st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon='🖥️')
+    st.sidebar.page_link("pages/20_Journalist_Home.py", label="System Admin", icon='🖥️')
     st.sidebar.page_link("pages/21_ML_Model_Mgmt.py", label='ML Model Management', icon='🏢')
 
 
@@ -49,30 +49,31 @@ def SideBarLinks(show_home=False):
         st.session_state.authenticated = False
         st.switch_page('Home.py')
         
-    if show_home:
-        # Show the Home page link (the landing page)
-        HomeNav()
+    # if show_home:
+    #     # Show the Home page link (the landing page)
+    #     HomeNav()
 
     # Show the other page navigators depending on the users' role.
     if st.session_state["authenticated"]:
 
         # Show World Bank Link and Map Demo Link if the user is a political strategy advisor role.
-        if st.session_state['role'] == 'pol_strat_advisor':
+        if st.session_state['role'] == 'investor':
             PolStratAdvHomeNav()
             WorldBankVizNav()
             MapDemoNav()
 
         # If the user role is usaid worker, show the Api Testing page
-        if st.session_state['role'] == 'usaid_worker':
+        if st.session_state['role'] == 'manager':
             PredictionNav()
             ApiTestNav() 
             ClassificationNav()
         
         # If the user is an administrator, give them access to the administrator pages
-        if st.session_state['role'] == 'administrator':
+        if st.session_state['role'] == 'journalist':
             AdminPageNav()
 
     # Always show the About page at the bottom of the list of links
+    
     AboutPageNav()
 
     if st.session_state["authenticated"]:
@@ -82,3 +83,5 @@ def SideBarLinks(show_home=False):
             del st.session_state['authenticated']
             st.switch_page('Home.py')
 
+    if not st.session_state["authenticated"]:
+        HomeNav()
