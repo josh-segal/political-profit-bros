cf6440f5528e62788b9e809235eb17a57dc6121c
tramseysalem <ramseysalem2@gmail.com>
2024-06-10 01:45:53 +0200

    adds name of journalist

--- app/src/pages/20_Journalist_Home.py
+++ app/src/pages/20_Journalist_Home.py
@@ -9,7 +9,10 @@ st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 
-st.title('Journalist Home Page')
+st.title(f"Welcome Journalist, {st.session_state['first_name']}.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
 
 if st.button('Inference Model', 
              type='primary',
