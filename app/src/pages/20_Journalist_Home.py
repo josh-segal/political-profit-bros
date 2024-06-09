2f8676992ca831f0bf14b621acbc445e7ee7cfb7
josh-segal <122911717+josh-segal@users.noreply.github.com>
2024-06-09 22:26:37 +0200

    legislation

--- app/src/pages/20_Journalist_Home.py
+++ app/src/pages/20_Journalist_Home.py
@@ -14,4 +14,10 @@ st.title('Journalist Home Page')
 if st.button('Inference Model', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/22_Model_Inference.py')
+    st.switch_page('pages/22_Model_Inference.py')
+
+if st.button('Search Legislation Database',
+             type='primary',
+             use_container_width=True,
+            help="Explore the legislation database"):
+    st.switch_page('pages/23_Legislation_Search.py')
