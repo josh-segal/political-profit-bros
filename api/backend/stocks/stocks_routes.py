b2561f90321b897652b3732015277eca310d8d73
josh-segal <122911717+josh-segal@users.noreply.github.com>
2024-06-10 02:17:39 +0200

    typo

--- api/backend/stocks/stocks_routes.py
+++ api/backend/stocks/stocks_routes.py
@@ -1,4 +1,4 @@
-rom flask import Blueprint, request, jsonify, make_response, current_app
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 import pandas as pd
 from backend.db_connection import db
